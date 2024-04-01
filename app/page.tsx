c3f5126881d46300602c89a38db5aa042a034275
nischay-chauhan <masternischay@outlook.com>
2024-04-01 18:04:11 +0530

    shadcn added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
 <div className="flex justify-center items-center h-screen">
   <h1 className="text-xl font-bold">Hwllo this is my streaming platform</h1>
+  <Button size={'lg'} variant={'secondary'}>SHadcn <button></button></Button>
 </div>
   );
 }
