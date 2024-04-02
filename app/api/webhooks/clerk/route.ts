d470f3d308fca2999f72311afdbdc444c2dcde21
nischay-chauhan <masternischay@outlook.com>
2024-04-02 23:15:11 +0530

    Added Clerk WebHook

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,53 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+
+export async function POST(req: Request) {
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    return new Response(
+      "Clerk Webhook Secret not found add the clerk secret to .env or .env.local",
+      { status: 500 }
+    );
+  }
+
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  if(!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Missing headers", { status: 400 });
+  }
+
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  const wh = new Webhook(WEBHOOK_SECRET);
+ 
+  let evt: WebhookEvent
+
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent
+  } catch (err) {
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
+      status: 400
+    })
+  }
+
+  const { id } = evt.data;
+  const eventType = evt.type;
+ 
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
+  console.log('Webhook body:', body)
+ 
+  return new Response('', { status: 200 })
+
+
+}
