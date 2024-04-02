d9e4dbd96c6ab83002fa6e63c404bd75aeee737d
nischay-chauhan <masternischay@outlook.com>
2024-04-02 19:14:34 +0530

    Logo

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: "700",
+});
+
+export const Logo = () => {
+    return (
+        <div className={cn("flex items-center gap-2")}>
+            <Image
+                src="/logo.svg"
+                alt="logo"
+                width={100}
+                height={100}
+                />
+            </div>
+    )
+}
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Logo } from './_components/logo'
 
 const AuthLayout = ({children} : {children : React.ReactNode}) => {
   return (
     <div className='flex gap-y-4 p-1 justify-center items-center h-screen'>
+      <Logo />
         {children}
     </div>
   )
