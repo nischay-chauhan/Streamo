0af8706a34f7f796ffc2690568cdd14d5635c15a
nischay-chauhan <masternischay@outlook.com>
2024-04-03 21:18:06 +0530

    Search bar : added state

--- app/(desktop)/_components/navbar/search.tsx
+++ app/(desktop)/_components/navbar/search.tsx
@@ -6,18 +6,46 @@ import { useRouter } from "next/navigation"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 export const Search = () => {
+    const router = useRouter();
+    const [value , setValue] = useState("")
+
+    const onSubmit = (e : React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(!value) return 
+        const url = qs.stringifyUrl({
+            url :  "/search",
+            query : {
+                term : value
+            },
+        },
+        {skipEmptyString : true})
+
+        router.push(url)
+    };
+
+    const onClear = () => {
+        setValue("")
+    }
+
     return(
         <>
         <form
         className="realtive w-full lg:w-[400px] flex items-center"
+        onSubmit={onSubmit}
         >
             <Input 
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
             placeholder="Search"
-            className="rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent
+            className="rounded-r-none 
+            focus-visible:ring-0 focus-visible:ring-transparent
             focus-visible:ring-offset-0
             " 
-
             />
+            {value && (<X 
+            className="absolute top-5.5 right-20 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition"
+            onClick={onClear}
+            />)}
             <Button type="submit" size={"sm"} variant={"secondary"} className="rounded-l-none" >
                 <SearchIcon className="h-5 w-5 text-muted-foreground"/>
             </Button>
