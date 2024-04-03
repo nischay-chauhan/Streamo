39993bc772e7a0ce947bee5f758d3440eb851d6f
nischay-chauhan <masternischay@outlook.com>
2024-04-03 19:56:57 +0530

    Clerk Request methods added in route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,33 +1,31 @@
-import { Webhook } from "svix";
-import { headers } from "next/headers";
-import { WebhookEvent } from "@clerk/nextjs/server";
-import { db } from "@/lib/db";
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
+
+import { db } from '@/lib/db'
+
 export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
-
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET
+ 
   if (!WEBHOOK_SECRET) {
-    return new Response(
-      "Clerk Webhook Secret not found add the clerk secret to .env or .env.local",
-      { status: 500 }
-    );
+    throw new Error('Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
-
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
-  if(!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Missing headers", { status: 400 });
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error occured -- no svix headers', {
+      status: 400
+    })
   }
 
-  const payload = await req.json();
+  const payload = await req.json()
   const body = JSON.stringify(payload);
-
   const wh = new Webhook(WEBHOOK_SECRET);
- 
-  let evt: WebhookEvent
 
+  let evt: WebhookEvent
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -42,19 +40,41 @@ export async function POST(req: Request) {
   }
 
   const eventType = evt.type;
-  if(eventType == 'user.created' || eventType == 'user.updated') {
-    console.log(evt.data);
+
+  if (eventType === "user.created") {
     await db.user.create({
       data: {
         externalUserId: payload.data.id,
-        username : payload.data.username,
-        imageUrl : payload.data.image_url
-      }
-    })
+        username: payload.data.username,
+        imageUrl: payload.data.image_url,
+        stream: {
+          create: {
+            name: `${payload.data.username}'s stream`,
+          },
+        },
+      },
+    });
   }
 
+  if (eventType === "user.updated") {
+    await db.user.update({
+      where: {
+        externalUserId: payload.data.id,
+      },
+      data: {
+        username: payload.data.username,
+        imageUrl: payload.data.image_url,
+      },
+    });
+  }
+ 
+  if (eventType === "user.deleted") {
+    await db.user.delete({
+      where: {
+        externalUserId: payload.data.id,
+      },
+    });
+  }
  
   return new Response('', { status: 200 })
-
-
-}
+};
