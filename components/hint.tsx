5b5235b2a9d2e42da21ac320fbde90ad6a615d1d
nischay-chauhan <masternischay@outlook.com>
2024-04-06 11:35:26 +0530

    Tool Tip

--- components/hint.tsx
+++ components/hint.tsx
@@ -1,2 +1,29 @@
 import  {Tooltip , TooltipContent , TooltipTrigger , TooltipProvider} from "@/components/ui/tooltip"
 
+interface HintProps {
+    label : string;
+    children: React.ReactNode;
+    asChild?: boolean;
+    side ?: "top" | "bottom" | "left" | "right";
+    align ?: "start" | "center" | "end"
+}
+
+export const Hint = ({label , children , asChild , side , align} : HintProps) => {
+
+    return (
+        <TooltipProvider>
+            <Tooltip delayDuration={0}>
+                <TooltipTrigger asChild>
+                    {children}
+                </TooltipTrigger>
+                <TooltipContent
+                sideOffset={5}
+                side={side}
+                align={align}
+                >
+                    {label}
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    )
+}
