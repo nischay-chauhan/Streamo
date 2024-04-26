7c432b0cd06c59165ef191df6a78c20d08b0de6f
nischay-chauhan <masternischay@outlook.com>
2024-04-26 10:29:14 +0530

    User Avatar

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -2,7 +2,7 @@ import { cva , type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import { Skeleton } from "./ui/skeleton";
 import { Avatar , AvatarFallback , AvatarImage } from "./ui/avatar";
-
+import { VariableIcon } from "lucide-react";
 interface UserAvatarProps {
     username : string;
     imageUrl : string;
@@ -10,10 +10,20 @@ interface UserAvatarProps {
     showBadge?: boolean;
 }
 
-export const UserAvatar = ({} : UserAvatarProps) => {
+export const UserAvatar = ({
+    username,
+    imageUrl,
+    isLive,
+    showBadge
+} : UserAvatarProps) => {
+    const canShowBadge = isLive && showBadge;
     return(
-        <div>
-            User Avatar 
+        <div className="relative">
+            <Avatar className={cn(
+                isLive && "ring ring-red-500 ring-offset-1",
+            )}>
+                <AvatarImage src={imageUrl} className="object-cover" alt="User Avatar" />
+            </Avatar>
         </div>
     )
 }
