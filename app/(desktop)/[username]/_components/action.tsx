134cdfc127dfc5b271828e2b56cfc1427a76ca50
nischay-chauhan <masternischay@outlook.com>
2024-05-04 16:44:37 +0530

    Follow toasts

--- app/(desktop)/[username]/_components/action.tsx
+++ app/(desktop)/[username]/_components/action.tsx
@@ -2,6 +2,7 @@
 import { Button } from "@/components/ui/button"
 import { OnFollow } from "@/actions/follow"
 import { useTransition } from "react"
+import { toast } from "sonner"
 
 
 interface isFollowingUserProps {
@@ -11,7 +12,7 @@ interface isFollowingUserProps {
 export const Action = ({ isFollowing }: isFollowingUserProps) => {
     const [isPending, startTranstion] = useTransition()
     const handleClick = () => {
-        startTranstion(() => { OnFollow("123") })
+        startTranstion(() => { OnFollow("123").then(() => toast.success("Followed Successfully")).catch(() => toast.error("Error following")) })
     }
     return (
         <Button disabled={isPending || isFollowing} onClick={handleClick} variant={'primary'}>
