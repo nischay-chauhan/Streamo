4ac64131a128238db9b94aff082bd060143754d0
nischay-chauhan <masternischay@outlook.com>
2024-05-29 17:20:32 +0530

    Dashboard TOggle Component'

--- app/(dashboard)/u/[username]/_components/sidebar/toggle.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/toggle.tsx
@@ -1,9 +1,47 @@
 "use client"
 
+import { Hint } from "@/components/hint"
+import { Button } from "@/components/ui/button"
+import { useCreatorSidebar } from "@/store/use-creator-sidebar"
+import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react"
+
 export const Toggle = () => {
+    const {collapsed , onExpand , onCollapse} = useCreatorSidebar((state) => state)
+
+    const label = collapsed ? "Expand" : "Collapse"
     return(
-        <div>
-            Toggle
-        </div>
+        <>
+        {collapsed && (
+            <div className="flex w-full items-center pt-4 mb-4 justify-center">
+                <Hint label={label} side="right" asChild>
+                    <Button
+                    onClick={onExpand}
+                    variant={"ghost"}
+                    className="h-auto p-2"
+                    >
+                        <ArrowRightFromLine className="h-4 w-4" />
+                    </Button>
+
+                </Hint>
+            </div>
+        )}
+
+        {!collapsed && (
+            <div className="p-3 pl-5 mb-2 flex items-center w-full">
+                <p className="font-semibold text-primary">
+                    Here u Go
+                </p>
+                <Hint label={label} side="right" asChild>
+                    <Button
+                    onClick={onCollapse}
+                    variant={"ghost"}
+                    className="h-auto p-2 ml-auto"
+                    >
+                        <ArrowLeftFromLine className="h-4 w-4" />
+                    </Button>
+                </Hint>
+            </div>
+        )}
+        </>
     )
 }
