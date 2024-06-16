79a9ac63d47cf71e4a34bebfdd393bc46a356867
nischay-chauhan <masternischay@outlook.com>
2024-06-16 14:26:51 +0530

    Live video player with fullscreen control

--- components/stream/live-video.tsx
+++ components/stream/live-video.tsx
@@ -1,13 +1,31 @@
 "use client"
-import { useRef } from "react";
+import { useRef , useState} from "react";
 import {useTracks} from "@livekit/components-react"
 import {Participant , Track} from "livekit-client"
+import { FullScreenControl } from "./fullscreen-control";
+import {useEventListener} from "usehooks-ts"
 interface LiveVideoProps {
     participants : Participant;
 }
 export const LiveVideo = ({participants}: LiveVideoProps) => {
     const videoRef = useRef<HTMLVideoElement>(null);
     const wrapperRef = useRef<HTMLDivElement>(null);
+    const [isFullscreen , setIsFullscreen] = useState(false)
+
+    const toggleFullscreen = () => {
+        if(isFullscreen){
+            document.exitFullscreen();
+        }else if(wrapperRef.current){
+            wrapperRef.current.requestFullscreen();
+        }
+    }
+
+    const handleFullScreenChange = () => {
+        const isCurrentlyFullscreen = document.fullscreenElement !== null;
+        setIsFullscreen(isCurrentlyFullscreen);
+    }
+
+    useEventListener("fullscreenchange" , handleFullScreenChange , wrapperRef)
 
     useTracks([
         Track.Source.Camera,
@@ -22,6 +40,16 @@ export const LiveVideo = ({participants}: LiveVideoProps) => {
         ref={wrapperRef}
         className="relative h-full flex">
            <video ref={videoRef} width={"100%"} />
+        <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
+            <div className="absolute bottom-0 flex h-14 items-center w-full justify-between bg-gradient-to-t from-neutral-900 px-4">
+                <FullScreenControl
+                isFullScreen={isFullscreen}
+                onToggle={toggleFullscreen} 
+                />
+
+            </div>
+
+        </div>
         </div>
     )
 }
