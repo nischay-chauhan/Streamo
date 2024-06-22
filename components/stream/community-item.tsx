1dd4b42b3b5db12840ceaf19d52a657f570a86ba
nischay-chauhan <masternischay@outlook.com>
2024-06-22 17:36:33 +0530

    Blocoing user in the community field

--- components/stream/community-item.tsx
+++ components/stream/community-item.tsx
@@ -11,13 +11,43 @@ interface CommunityItemProps{
     hostName : string
     viewerName : string
     participantName?: string
-    participantIdentity?: string
+    participantIdentity: string
 }
 
 export const CommunityItem = ({hostName , viewerName , participantName , participantIdentity }: CommunityItemProps) => {
+    const [isPending , startTransition] = useTransition();
+    const color = stringToColor(participantName || "");
+    const isSelf = participantName === viewerName
+    const isHost = viewerName === hostName
+
+    const handleBlock = () => {
+        if(!participantName || isSelf || !isHost){
+            return;
+        }
+        startTransition(() => {
+            onBlock(participantIdentity).then(() => toast.success(`You have blocked ${participantName}`)).catch(() => toast.error("Error following"))
+        })
+    }
     return(
-        <div className="flex items-center gap-x-2">
+        <div className={cn("group flex items-center justify-center w-full p-2 rounded-md text-sm hover:bg-white/5" , isPending && "opacity-50 pointer-events-none")}>
+            <p style={{color : color}}>
             {participantName}
+            </p>
+            {isHost && !isSelf && (
+                <Hint
+                label="Block"
+                >
+                    <Button 
+                    variant={"ghost"}
+                    disabled={isPending}
+                    onClick={handleBlock}
+                    className="h-auto transition w-auto opacity-0 group-hover:opacity-100 p-1 ">
+                        <MinusCircle
+                        className="h-4 w-4 text-muted-foreground"
+                        />
+                    </Button>
+                </Hint>
+            )}
         </div>
     )
 }
